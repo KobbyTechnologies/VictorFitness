e3da0c030ec67aac284c1bb4ff8bc7bd0fb66cc3
enockabere <maebaenock95@gmail.com>
2021-12-23 12:13:32 +0300

    deplyment

--- landing/views.py
+++ landing/views.py
@@ -2,23 +2,23 @@ from django.shortcuts import redirect, render
 
 # Create your views here.
 def landing (request):
-    if request.method == 'POST':
-        weight = int(request.POST['weight'])
-        height = int(request.POST['height'])
-        if height >0 and weight > 0:
-            res = height/3.281
-            BMI = weight/(res*res)
-            new_bmi= round(BMI,3)
-            if new_bmi < 18.5:
-                myBMI = (f"your BMI is: {new_bmi} and you're in the underweight range")
-            elif new_bmi >18.5 and new_bmi <=24.9:
-                myBMI = (f"your BMI is: {new_bmi} and you're in the healthy weight range")
-            elif new_bmi >=25 and new_bmi <=29.9:
-                myBMI=(f"your BMI is: {new_bmi} and you're in the overweight range")
-            else:
-                myBMI = (f"your BMI is: {new_bmi} and you're in the obese range")
-        else:
-            myBMI= ("Invalid Entry")
-    ctx = {"BMI":myBMI}
-    return render (request, 'main/landing.html',ctx)
+    # if request.method == 'POST':
+    #     weight = int(request.POST['weight'])
+    #     height = int(request.POST['height'])
+    #     if height >0 and weight > 0:
+    #         res = height/3.281
+    #         BMI = weight/(res*res)
+    #         new_bmi= round(BMI,3)
+    #         if new_bmi < 18.5:
+    #             myBMI = (f"your BMI is: {new_bmi} and you're in the underweight range")
+    #         elif new_bmi >18.5 and new_bmi <=24.9:
+    #             myBMI = (f"your BMI is: {new_bmi} and you're in the healthy weight range")
+    #         elif new_bmi >=25 and new_bmi <=29.9:
+    #             myBMI=(f"your BMI is: {new_bmi} and you're in the overweight range")
+    #         else:
+    #             myBMI = (f"your BMI is: {new_bmi} and you're in the obese range")
+    #     else:
+    #         myBMI= ("Invalid Entry")
+    # ctx = {"BMI":myBMI}
+    return render (request, 'main/landing.html')
 
