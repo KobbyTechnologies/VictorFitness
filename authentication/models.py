91e1958763bd5b37b2b6f07767b08d2ba24e7ed3
enockabere <maebaenock95@gmail.com>
2022-03-30 17:06:20 +0300

    create user models

--- authentication/models.py
+++ authentication/models.py
@@ -26,3 +26,27 @@ class MyUserManager(BaseUserManager):
         user.is_superuser = True
         user.save(using=self._db)
         return user
+    
+class MyUser(AbstractBaseUser):
+    email = models.EmailField(verbose_name="Email", max_length=60, unique=True)
+    username = models.CharField(verbose_name="Username", max_length=200)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = 'email'
+    
+    REQUIRED_FIELDS = ['username']
+    
+    objects = MyUserManager()
+    
+    def __str__(self):
+        return str(self.username)
+    
+    def has_perm(self,perm,obj=None):
+        return True
+    def has_module_perms(self, app_label):
+        return True
