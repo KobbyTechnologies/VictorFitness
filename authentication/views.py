99ac12d70e6d93fabd9c9649f36c4521346af0f8
enockabere <maebaenock95@gmail.com>
2022-03-30 17:10:55 +0300

    create register route

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from  django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
 # Create your views here.
+def register_request(request):
+    context={}
+    if request.method == "POST":
+        try:
+            form=request.POST.get('data')
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Registration successful." )
+                print("success")
+                return redirect('login')
+        except Exception as e:
+            messages.error(request, "Unsuccessful registration. Invalid information.")
+            print(e)
+        context['register_form']=form
+    else:
+        context['register_form']=form
+    return render(request,'accounts/register.html',context)
